3a033b17cbcd365d970075dd1b70afceaf377ab1
Davide Garberi <dade.garberi@gmail.com>
2024-04-18 21:43:20 +0200

    app: SyncUtils: Reverse playlists order
     * This is needed to match the chronological order from YouTube

--- app/src/main/java/com/dd3boh/outertune/utils/SyncUtils.kt
+++ app/src/main/java/com/dd3boh/outertune/utils/SyncUtils.kt
@@ -120,10 +120,10 @@ class SyncUtils @Inject constructor(
 
     suspend fun syncSavedPlaylists() {
         YouTube.library("FEmusic_liked_playlists").completedLibraryPage().onSuccess { page ->
-            val playlistList = page.items.filterIsInstance<PlaylistItem>()
+            val playlistList = page.items.filterIsInstance<PlaylistItem>().drop(1).reversed()
             val dbPlaylists = database.playlistsByNameAsc().first()
 
-            playlistList.drop(1).forEach { playlist ->
+            playlistList.forEach { playlist ->
                 var playlistEntity = dbPlaylists.find { playlist.id == it.playlist.browseId }?.playlist
                 if (playlistEntity == null) {
                     playlistEntity = PlaylistEntity(
