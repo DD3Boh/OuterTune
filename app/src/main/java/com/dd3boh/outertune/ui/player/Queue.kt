70b35be874405d5d43d4ba814056082d79093d76
mikooomich <mikooomichael@gmail.com>
2024-06-05 13:24:37 -0400

    app: Queue: Import mutable state
    * Compiles now

--- app/src/main/java/com/dd3boh/outertune/ui/player/Queue.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/player/Queue.kt
@@ -54,9 +54,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
