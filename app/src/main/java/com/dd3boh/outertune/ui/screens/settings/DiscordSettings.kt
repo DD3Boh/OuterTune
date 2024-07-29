bbb18b22c1077c21eacffb42a3d890c1b977e8b8
Rusk2106 <110672921+tangles2134@users.noreply.github.com>
2024-07-29 16:43:40 -0500

    Added Discord Settings page

---
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/settings/DiscordSettings.kt
@@ -0,0 +1,166 @@
+package com.dd3boh.outertune.ui.screens.settings
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Language
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.Lyrics
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import com.dd3boh.outertune.LocalPlayerAwareWindowInsets
+import com.dd3boh.outertune.R
+import com.dd3boh.outertune.constants.AccountChannelHandleKey
+import com.dd3boh.outertune.constants.AccountEmailKey
+import com.dd3boh.outertune.constants.AccountNameKey
+import com.dd3boh.outertune.constants.ContentCountryKey
+import com.dd3boh.outertune.constants.ContentLanguageKey
+import com.dd3boh.outertune.constants.CountryCodeToName
+import com.dd3boh.outertune.constants.InnerTubeCookieKey
+import com.dd3boh.outertune.constants.LanguageCodeToName
+import com.dd3boh.outertune.constants.LyricTrimKey
+import com.dd3boh.outertune.constants.ProxyEnabledKey
+import com.dd3boh.outertune.constants.ProxyTypeKey
+import com.dd3boh.outertune.constants.ProxyUrlKey
+import com.dd3boh.outertune.constants.SYSTEM_DEFAULT
+import com.dd3boh.outertune.ui.component.EditTextPreference
+import com.dd3boh.outertune.ui.component.IconButton
+import com.dd3boh.outertune.ui.component.ListPreference
+import com.dd3boh.outertune.ui.component.PreferenceEntry
+import com.dd3boh.outertune.ui.component.PreferenceGroupTitle
+import com.dd3boh.outertune.ui.component.SwitchPreference
+import com.dd3boh.outertune.ui.utils.backToMain
+import com.dd3boh.outertune.utils.rememberEnumPreference
+import com.dd3boh.outertune.utils.rememberPreference
+import com.zionhuang.innertube.utils.parseCookieString
+import java.net.Proxy
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContentSettings(
+    navController: NavController,
+    scrollBehavior: TopAppBarScrollBehavior,
+) {
+    val accountName by rememberPreference(AccountNameKey, "")
+    val accountEmail by rememberPreference(AccountEmailKey, "")
+    val accountChannelHandle by rememberPreference(AccountChannelHandleKey, "")
+    val innerTubeCookie by rememberPreference(InnerTubeCookieKey, "")
+    val isLoggedIn = remember(innerTubeCookie) {
+        "SAPISID" in parseCookieString(innerTubeCookie)
+    }
+    val (ytmSync, onYtmSyncChange) = rememberPreference(LyricTrimKey, defaultValue = true)
+    val (contentLanguage, onContentLanguageChange) = rememberPreference(key = ContentLanguageKey, defaultValue = "system")
+    val (contentCountry, onContentCountryChange) = rememberPreference(key = ContentCountryKey, defaultValue = "system")
+    val (proxyEnabled, onProxyEnabledChange) = rememberPreference(key = ProxyEnabledKey, defaultValue = false)
+    val (proxyType, onProxyTypeChange) = rememberEnumPreference(key = ProxyTypeKey, defaultValue = Proxy.Type.HTTP)
+    val (proxyUrl, onProxyUrlChange) = rememberPreference(key = ProxyUrlKey, defaultValue = "host:port")
+
+
+    Column(
+        Modifier
+            .windowInsetsPadding(LocalPlayerAwareWindowInsets.current)
+            .verticalScroll(rememberScrollState())
+    ) {
+        PreferenceGroupTitle(
+            title = "ACCOUNT"
+        )
+        PreferenceEntry(
+            title = { Text(if (isLoggedIn) accountName else stringResource(R.string.login)) },
+            description = if (isLoggedIn) {
+                accountEmail.takeIf { it.isNotEmpty() }
+                    ?: accountChannelHandle.takeIf { it.isNotEmpty() }
+            } else null,
+            icon = { Icon(Icons.Rounded.Person, null) },
+            onClick = { navController.navigate("login") }
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.ytm_sync)) },
+            icon = { Icon(Icons.Rounded.Lyrics, null) },
+            checked = ytmSync,
+            onCheckedChange = onYtmSyncChange,
+            isEnabled = isLoggedIn
+        )
+
+        PreferenceGroupTitle(
+            title = "LOCALIZATION"
+        )
+        ListPreference(
+            title = { Text(stringResource(R.string.content_language)) },
+            icon = { Icon(Icons.Rounded.Language, null) },
+            selectedValue = contentLanguage,
+            values = listOf(SYSTEM_DEFAULT) + LanguageCodeToName.keys.toList(),
+            valueText = {
+                LanguageCodeToName.getOrElse(it) {
+                    stringResource(R.string.system_default)
+                }
+            },
+            onValueSelected = onContentLanguageChange
+        )
+        ListPreference(
+            title = { Text(stringResource(R.string.content_country)) },
+            icon = { Icon(Icons.Rounded.LocationOn, null) },
+            selectedValue = contentCountry,
+            values = listOf(SYSTEM_DEFAULT) + CountryCodeToName.keys.toList(),
+            valueText = {
+                CountryCodeToName.getOrElse(it) {
+                    stringResource(R.string.system_default)
+                }
+            },
+            onValueSelected = onContentCountryChange
+        )
+
+        PreferenceGroupTitle(
+            title = "PROXY"
+        )
+
+        SwitchPreference(
+            title = { Text(stringResource(R.string.enable_proxy)) },
+            checked = proxyEnabled,
+            onCheckedChange = onProxyEnabledChange
+        )
+
+        if (proxyEnabled) {
+            ListPreference(
+                title = { Text(stringResource(R.string.proxy_type)) },
+                selectedValue = proxyType,
+                values = listOf(Proxy.Type.HTTP, Proxy.Type.SOCKS),
+                valueText = { it.name },
+                onValueSelected = onProxyTypeChange
+            )
+            EditTextPreference(
+                title = { Text(stringResource(R.string.proxy_url)) },
+                value = proxyUrl,
+                onValueChange = onProxyUrlChange
+            )
+        }
+    }
+
+    TopAppBar(
+        title = { Text(stringResource(R.string.content)) },
+        navigationIcon = {
+            IconButton(
+                onClick = navController::navigateUp,
+                onLongClick = navController::backToMain
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Rounded.ArrowBack,
+                    contentDescription = null
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
