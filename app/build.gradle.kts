70185e230682d56ee366f3ee58f74498e5578f91
mikooomich <mikooomichael@gmail.com>
2024-08-30 16:19:23 -0400

    gradle: Version bump to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20
-        versionName = "0.6.0-rc1"
+        versionCode = 21
+        versionName = "0.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
