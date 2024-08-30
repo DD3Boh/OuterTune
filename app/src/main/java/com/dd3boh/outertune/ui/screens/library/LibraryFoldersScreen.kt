77df70bf687dac20fcfb6c839c65ec636eb9deaf
mikooomich <mikooomichael@gmail.com>
2024-08-30 14:58:49 -0400

    ui: Revert song & folders sticky header
    * Having it integrated with the list was clearly the wrong way to do it, and is too thick for landscape

--- app/src/main/java/com/dd3boh/outertune/ui/screens/library/LibraryFoldersScreen.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/library/LibraryFoldersScreen.kt
@@ -10,14 +10,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -179,14 +175,12 @@ fun LibraryFoldersScreen(
             state = lazyListState,
             contentPadding = LocalPlayerAwareWindowInsets.current.asPaddingValues()
         ) {
-            stickyHeader(
+            item(
                 key = "header",
                 contentType = CONTENT_TYPE_HEADER
             ) {
                 Column(
-                    modifier = Modifier
-                        .background(MaterialTheme.colorScheme.background)
-                        .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Top))
+                    modifier = Modifier.background(MaterialTheme.colorScheme.background)
                 ) {
                     filterContent?.let {
                         it()
